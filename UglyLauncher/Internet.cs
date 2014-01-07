e69679f9b6d9fde10e4e1f6d5146655a2da0c878
0uti <outi@outi-networks.de>
2014-01-07 21:46:28 +0100

    initital project upload
    empty Windows Forms Project with http wrapper class

---
+++ UglyLauncher/Internet.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace Internet
+{
+    public static class Http
+    {
+        public static string GET(string url)
+        {
+            WebRequest request = WebRequest.Create(url);
+            WebResponse response = request.GetResponse();
+            StreamReader stringResponse = new StreamReader(response.GetResponseStream());
+            return stringResponse.ReadToEnd().Trim();
+        }
+
+        public static string POST(string url, string postdata, string contenttype)
+        {
+            WebRequest request = WebRequest.Create(url);
+            request.Method = "POST";
+            byte[] byteArray = Encoding.UTF8.GetBytes(postdata);
+            request.ContentType = contenttype;
+            request.ContentLength = byteArray.Length;
+            Stream dataStream = request.GetRequestStream();
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            dataStream.Close();
+            WebResponse response = request.GetResponse();
+            StreamReader stringResponse = new StreamReader(response.GetResponseStream());
+            return stringResponse.ReadToEnd().Trim();
+        }
+    }
+}
---
+++ UglyLauncher/Minecraft.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+using Internet;
+
+namespace Minecraft
+{
+
+    public static class Launcher
+    {
+        private static string appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+    }
+
+
+    public static class Authentication
+    {
+
+    }
+}
